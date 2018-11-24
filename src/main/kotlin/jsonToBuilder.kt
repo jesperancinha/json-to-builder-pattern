2aeb41818c47d2f9dbda3d4b261cad48bb764033
Joao Esperancinha <jofisaes@gmail.com>
2018-11-24 13:44:06 +0100

    JE-JTBP-01 basic functions implemented

---
+++ src/main/kotlin/jsonToBuilder.kt
@@ -0,0 +1,34 @@
+import com.google.gson.GsonBuilder
+import java.io.BufferedReader
+import java.io.BufferedWriter
+
+fun main(args: Array<String>) {
+
+}
+
+private fun readLinesFromBuffer(bufferedReader: BufferedReader, bufferedWriter: BufferedWriter) {
+    val readLines: List<String> = bufferedReader.readLines()
+    readLines.forEach { line -> bufferedWriter.write(convertToBuilder(line)) }
+
+}
+
+fun convertToBuilder(line: String): String {
+    val sb = StringBuilder()
+    sb.append("ParentBuilder.create()")
+    GsonBuilder().create().fromJson(line, HashMap::class.java)?.forEach { (key, value) ->
+        sb.append(".$key".replace("_","").replace("class","clazz"))
+        if (isNumeric(value.toString()) || key.toString().contains("Type")) {
+            sb.append("($value)")
+        } else {
+            sb.append("(\"$value\")")
+        }
+    }
+    sb.append(".build();")
+    System.out.println(sb.toString())
+    return sb.toString()
+}
+
+fun isNumeric(value: String): Boolean {
+    return value.matches("-?\\d+(\\.\\d+)?".toRegex())
+}
+
